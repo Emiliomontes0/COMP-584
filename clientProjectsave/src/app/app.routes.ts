7244df7849e8a4dafe15a1e7022383347aa54e8e
Montes <emilio.montes.614@my.csun.edu>
2025-02-07 18:25:18 -0800

    client fix

---
+++ clientProjectsave/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+import { CitiesComponent } from './cities/cities.component';
+import { CountriesComponent } from './countries/countries.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+export const routes: Routes = [
+    {
+        path: "weather", component: WeatherComponent 
+    },
+
+    {
+        path: "navbar", component: NavBarComponent
+    },
+
+    {
+        path: "cities", component: CitiesComponent
+    },
+
+    {
+        path: "countries", component: CountriesComponent
+    },
+    {
+        path: "", component: WeatherComponent, pathMatch: "full"
+    }
+];
