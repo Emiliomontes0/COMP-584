cd1e3770e39e39566c99bf26b606a4f636ba6f10
Montes <emilio.montes.614@my.csun.edu>
2025-02-21 18:19:12 -0800

    db server

---
+++ serverproject/modeldb/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace modelDB;
+
+[Table("city")]
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("city")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string City1 { get; set; } = null!;
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [Column("countryid")]
+    public int Countryid { get; set; }
+
+    [ForeignKey("Countryid")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ serverproject/modeldb/WorldCitySourceContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace modelDB;
+
+public partial class WorldCitySourceContext : DbContext
+{
+    public WorldCitySourceContext()
+    {
+    }
+
+    public WorldCitySourceContext(DbContextOptions<WorldCitySourceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb; database= WorldCitySource");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Cities)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_city_country");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_single");
+
+            entity.Property(e => e.Iso2).IsFixedLength();
+            entity.Property(e => e.Iso3).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
